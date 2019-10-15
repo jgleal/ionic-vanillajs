e2d3d6802728700a4dde5dd3cba5bf839505d1c9
JGLeal <jgleal@gmail.com>
2019-10-15 10:47:46 +0200

    adds mapea

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
-import config from './app-config'
-import './global.css'
-import ViewHome from './routes/home'
-import ViewMap from './routes/map'
-import ViewAbout from './routes/about'
+import config from './app-config';
+import './global.css';
+import ViewHome from './routes/home';
+import ViewMap from './routes/map';
+import ViewAbout from './routes/about';
 
-console.info(`${config.name}@${config.version}`)
+console.info(`${config.name}@${config.version}`);
 
-//attachs cordova deviceready event to access device functions 
-document.addEventListener("deviceready", onDeviceReady, false);
+//attachs cordova deviceready event to access device functions
+document.addEventListener('deviceready', onDeviceReady, false);
 function onDeviceReady() {
-    config.isApp = true
-    //here can use device functions    
+  config.isApp = true;
+  //here can use device functions
 }
 
 //registers custom elements (routes & components)
-window.customElements.define('view-home', ViewHome)
-window.customElements.define('view-map', ViewMap)
-window.customElements.define('view-about', ViewAbout)
+window.customElements.define('view-home', ViewHome);
+window.customElements.define('view-map', ViewMap);
+window.customElements.define('view-about', ViewAbout);
