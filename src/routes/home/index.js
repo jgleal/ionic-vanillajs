126c8e67c6e796c5cb0288bd6d23121fab41d699
JGLeal <jgleal@gmail.com>
2019-10-15 12:15:04 +0200

    map-card & resizeobserver

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -3,13 +3,24 @@ import './home.css'
 import config from '@/app-config'
 
 export default class ViewHome extends HTMLElement {
+  constructor() {
+    super()
+    this.sizeObserver = new ResizeObserver(() => {
+      if (this.olMap) {
+        this.olMap.updateSize()
+      }
+    })
+  }
   connectedCallback() {
     this.innerHTML = html
     const controller = this.querySelector('ion-alert-controller')
     const button = this.querySelector('#btnAlert')
     button.controller = controller
     button.addEventListener('click', this.handleButtonClick, false)
+    this.sizeObserver.observe(this)
+    this._createMap()
   }
+
   handleButtonClick(evt) {
     let contentMessage = `<p class="alert-footer"><small>Template made by <a href="https://github.com/jgleal">JGLeal</a><small></p>`
     if (config.isApp) {
@@ -37,5 +48,43 @@ export default class ViewHome extends HTMLElement {
       .then(alert => {
         alert.present()
       })
+  }
+
+  _createMap() {
+    let mMap = M.map({
+      container: 'map-card',
+      wmcfile: [
+        'http://mapea4-sigc.juntadeandalucia.es/files/wmc/satelite.xml*Satelite',
+      ],
+      center: [-3.587692, 37.175766],
+      zoom: 13,
+      projection: 'EPSG:4326*d',
+    })
+
+    let puntosInteres = new M.layer.GeoJSON({
+      source: {
+        features: [
+          {
+            properties: {
+              'Dirección: ': 'Calle Real de la Alhambra, s/n, 18009 Granada',
+              'Coordenadas: ': '-3.587692, 37.175766',
+            },
+            type: 'Feature',
+            geometry: {
+              type: 'Point',
+              coordinates: [-3.587692, 37.175766],
+            },
+          },
+        ],
+        type: 'FeatureCollection',
+      },
+      name: 'Puntos de Interés',
+    })
+    mMap.addLayers(puntosInteres)
+    console.log(mMap.getFeatureHandler())
+    puntosInteres.on(M.evt.SELECT_FEATURES, features => {
+      console.log(features)
+    })
+    this.olMap = mMap.getMapImpl()
   }
 }
--- src/routes/map/index.js
+++ src/routes/map/index.js
@@ -1,11 +1,21 @@
 import html from './map.html'
 
 export default class ViewMap extends HTMLElement {
+  constructor() {
+    super()
+    this.sizeObserver = new ResizeObserver(() => {
+      if (this.olMap) {
+        this.olMap.updateSize()
+      }
+    })
+  }
   connectedCallback() {
     this.innerHTML = html
     let mMap = new M.map({
-      container: this.querySelector('#map'),
+      container: 'map',
       controls: ['scale'],
     })
+    this.olMap = mMap.getMapImpl()
+    this.sizeObserver.observe(this)
   }
 }
