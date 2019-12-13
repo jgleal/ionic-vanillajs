b982b3299f511f22c6a892c0a348c526a633695c
JGLeal <jgleal@gmail.com>
2019-12-13 12:49:25 +0100

    add combo provincia

--- src/routes/map/index.js
+++ src/routes/map/index.js
@@ -1,4 +1,5 @@
 import html from './map.html'
+const ZOOM_PROV = 6
 
 export default class ViewMap extends HTMLElement {
   constructor() {
@@ -11,7 +12,13 @@ export default class ViewMap extends HTMLElement {
   }
   connectedCallback() {
     this.innerHTML = html
-    let mMap = new M.map({
+    this._selectProv = this.querySelector('#selectProv')
+
+    this._selectProv.addEventListener('ionChange', evt => {
+      this.mMap.setCenter(evt.detail.value).setZoom(ZOOM_PROV)
+    })
+
+    this.mMap = new M.map({
       container: 'map',
       controls: ['scale'],
     })
