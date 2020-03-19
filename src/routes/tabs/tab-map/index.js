41d75a95aa919e23ea72f20621fc639113cce105
JGLeal <jgleal@gmail.com>
2020-03-19 17:24:41 +0100

    un/select features from list

--- src/routes/tabs/tab-map/index.js
+++ src/routes/tabs/tab-map/index.js
@@ -1,5 +1,22 @@
 import html from './tab-map.html'
-
+const featureStyle = new M.style.Point({
+  radius: 5,
+  fill: {
+    color: 'green',
+  },
+  stroke: {
+    color: '#000',
+  },
+})
+const featureSelStyle = new M.style.Point({
+  radius: 7,
+  fill: {
+    color: 'yellow',
+  },
+  stroke: {
+    color: '#000',
+  },
+})
 export default class ViewTabMap extends HTMLElement {
   constructor() {
     super()
@@ -28,25 +45,40 @@ export default class ViewTabMap extends HTMLElement {
   async _generateLayer() {
     const geoData = await this._parent.getData()
     console.log(geoData)
-    let lyGeo = new M.layer.GeoJSON({
-      name: 'capaJson',
+    this.lyGeo = new M.layer.GeoJSON({
+      name: 'Fuentes de Andalucía',
       source: geoData,
     })
-    lyGeo.setStyle(
-      new M.style.Point({
-        radius: 5,
-        fill: {
-          color: 'green',
-        },
-        stroke: {
-          color: '#000',
-        },
+    this.lyGeo.on(M.evt.SELECT_FEATURES, features => {
+      features.forEach(f => {
+        f.setStyle(featureSelStyle)
       })
-    )
-    this.mMap.addLayers(lyGeo)
+    })
+    this.lyGeo.on(M.evt.UNSELECT_FEATURES, features => {
+      features.forEach(f => {
+        f.setStyle(featureStyle)
+      })
+    })
+
+    this.lyGeo.setStyle(featureStyle)
+
+    this.mMap.addLayers(this.lyGeo)
   }
 
   showElement(element) {
-    console.log(element)
+    const coordsFeature = element.geometry.coordinates
+    this.mMap.setCenter(coordsFeature).setZoom(12)
+    const f = this.lyGeo.getFeatureById(element.id)
+    const featuresHandler = this.mMap.getFeatureHandler()
+    const featuresSelected =
+      featuresHandler.prevSelectedFeatures_[this.lyGeo.name]
+    if (featuresSelected.length > 0) {
+      featuresHandler.unselectFeatures(featuresSelected, this.lyGeo, {})
+      this.mMap.removePopup()
+    }
+    featuresHandler.selectFeatures([f], this.lyGeo, {
+      coord: coordsFeature,
+    })
+    window.location.href = '#/tabs/tab-map'
   }
 }
